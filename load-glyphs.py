740662eb7d82edfad45c06f712ee85cd7d04de1f
Mercury13 <mercury13@ukr.net>
2025-11-23 22:34:45 +0200

    ex 133A7

--- load-glyphs.py
+++ load-glyphs.py
@@ -283,7 +283,7 @@ GLYPH_SIZES = {
     0x13385: MEDIUM,  # hobble + crossbar
     0x133A3: MEDWIDE,  # frail
     0x133A4: SMALL,  # some bag
-    0x133A6: SMALL, 0x133A7: SMALL,  # another bag
+    0x133A6: SMALL, 0x133A7: MEDIUM,  # another bag
     0x133A9: MEDIUM,  # bandage
     # W: vessels
     0x133B6: SMALL,  # some really craggy vessel
